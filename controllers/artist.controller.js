db2f1f054aa03d2ed299a70cf53b7a7a6c5cce78
cabrera-evil <douglascabrera.dev@gmail.com>
2024-01-04 23:36:56 -0600

    Refactor: artist controller

--- controllers/artist.controller.js
+++ controllers/artist.controller.js
@@ -1,8 +1,8 @@
-const arstistService = require('../services/artist.service');
+const artistService = require('../services/artist.service');
 
 const createArtist = async (req, res) => {
     try {
-        const artist = await arstistService.createArtist(req.body);
+        const artist = await artistService.createArtist(req.body);
         res.status(201).json({ message: 'Artist created', data: artist });
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -11,7 +11,7 @@ const createArtist = async (req, res) => {
 
 const getArtists = async (req, res) => {
     try {
-        const artists = await arstistService.getArtists();
+        const artists = await artistService.getArtists();
         res.status(200).json({ message: 'Artists retrieved', data: artists });
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -20,7 +20,7 @@ const getArtists = async (req, res) => {
 
 const getArtistById = async (req, res) => {
     try {
-        const artist = await arstistService.getArtistById(req.params.id);
+        const artist = await artistService.getArtistById(req.params.id);
         res.status(200).json({ message: 'Artist retrieved', data: artist });
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -29,7 +29,7 @@ const getArtistById = async (req, res) => {
 
 const updateArtist = async (req, res) => {
     try {
-        const artist = await arstistService.updateArtist(req.params.id, req.body);
+        const artist = await artistService.updateArtist(req.params.id, req.body);
         res.status(200).json({ message: 'Artist updated', data: artist });
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -38,7 +38,7 @@ const updateArtist = async (req, res) => {
 
 const deleteArtist = async (req, res) => {
     try {
-        const artist = await arstistService.deleteArtist(req.params.id);
+        const artist = await artistService.deleteArtist(req.params.id);
         res.status(200).json({ message: 'Artist deleted', data: artist });
     } catch (error) {
         res.status(500).json({ message: error.message });
